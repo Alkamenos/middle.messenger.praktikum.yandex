e21e0bf38344e6f4fb0e7bcde254413ee82f6d9f
Leonid Artemev <alkamenos@gmail.com>
2021-03-22 11:39:24 +0500

    fix a=>buttom

--- static/pages/registration/registration.js
+++ static/pages/registration/registration.js
@@ -1,1 +1,11 @@
 import './registration.scss';
+
+window.onload = () => {
+	const registrationForm = document.querySelector('.registration-form');
+	registrationForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const fd = new FormData(registrationForm);
+		console.log(Object.fromEntries(fd.entries()));
+		window.location.href = '/pages/chat/chat.html';
+	});
+};
