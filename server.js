ff0c4ebd7dd6618862e46ae943d4dfb8664be061
leonidartemev <me@artemev.it>
2021-08-21 21:43:44 +0500

    fix

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const path = require('path');
 
 app.use(express.static('./dist'));
 
+app.use('*', (req, res) => {
+	res.redirect('/');
+});
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + './dist/index.html');
 });
