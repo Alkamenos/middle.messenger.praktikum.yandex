b4e9ad48110e55288ec67adfe5232e9d136d3d71
leonidartemev <me@artemev.it>
2021-08-25 05:07:30 +0500

    добавил husky, webpack, docker выполнил npm audit fix

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    target: "node",
+    entry: './src/' +
+        'index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'messenger.bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    devServer: {
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                        },
+                    },
+                ],
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+
+    ],
+};
