d2c364129d7bc914bb028130d2a178e2345d7ba3
leonidartemev <me@artemev.it>
2021-08-31 13:59:55 +0500

    fix build path

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static("./dist"));
+app.use(express.static("./build"));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
